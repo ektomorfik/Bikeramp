6c6b6c45c70cf4146b8a0085dde8c1415adefe13
ektomorfik <badziokonrad@gmail.com>
2021-07-02 16:00:07 +0200

    feat: introduce module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { StatisticsModule } from './statistics/statistics.module';
+import { TripModule } from './trip/trip.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TripModule, StatisticsModule],
 })
 export class AppModule {}
